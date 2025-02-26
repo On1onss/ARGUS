b3bf95de115c0bf5a89e1ec278cd9d3e076f771a
On1onss <sub.on1ons@gmail.com>
2025-02-26 11:49:36 +0300

    Add token check. Example profile route

--- server/api/routes/pages.py
+++ server/api/routes/pages.py
@@ -3,15 +3,27 @@ from pathlib import Path
 from fastapi import APIRouter
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
-from fastapi.responses import Response
+from fastapi.responses import Response, HTMLResponse
 
 from config import settings
-from starlette.responses import HTMLResponse, RedirectResponse, JSONResponse
+from api.routes.auth import get_current_user
 
 router = APIRouter(tags=["Welcome Page"])
 templates = Jinja2Templates(directory=str(Path(settings.BASE_DIR, 'src/pages/')))
 
 
 @router.get("/login", response_class=HTMLResponse)
-async def index(request: Request) -> Response:
+async def login(request: Request) -> Response:
     return templates.TemplateResponse("home/login.html", {"request": request, }, )
+
+
+@router.get("/profile")
+async def profile(request: Request) -> dict:
+    token = request.cookies.get("token").split()
+    user = await get_current_user(token[1])
+
+    if user["is_admin"]:
+        return {
+            "message": "Hello",
+        }
+
