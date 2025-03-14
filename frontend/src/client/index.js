ea0f896aba082d802c52d8b5d6caa6c7b4c80841
On1onss <sub.on1ons@gmail.com>
2025-03-14 18:35:28 +0300

    Update get data func

--- frontend/src/client/index.js
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export { ApiError } from './core/ApiError';
-export { CancelablePromise, CancelError } from './core/CancelablePromise';
-export { OpenAPI } from './core/OpenAPI';
-export * from "./sdk.gen";
-export * from "./types.gen";
