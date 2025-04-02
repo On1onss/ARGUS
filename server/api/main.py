a3f798ac2f28538d3d5f43a8152eb67662074a8c
On1onss <sub.on1ons@gmail.com>
2025-04-02 18:45:47 +0300

    add health_check.py

--- server/api/main.py
+++ server/api/main.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from api.routes import auth, chart, pages
+from api.routes import auth, chart, pages, health_check
 
 api_router = APIRouter()
 api_router.include_router(auth.router)
 api_router.include_router(chart.router)
 api_router.include_router(pages.router)
+api_router.include_router(health_check.router)
---
+++ server/api/routes/health_check.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+import json
+import httpx
+import time
+
+from fastapi import APIRouter
+
+
+router = APIRouter(prefix="/health_check", tags=["Health"])
+
+
+@router.get("/{host}")
+def health_check(host):
+    t0 = time.time()
+    try:
+        response = httpx.head(f'https://{host}/')
+        # if response.json()["error"]:
+        #     return json.dumps({"error": "Host is not available"})
+        result = {
+            "status code": response.status_code,
+            "value": f"{(time.time() - t0)*1000:.1f}",
+            "time": datetime.now().strftime("%H:%M:%S")
+        }
+        return result
+    except httpx.HTTPError:
+        return {"error": "Host is not available"}
+
+
